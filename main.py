e14db7b5d827dd02bb12261b80a44d10b87edf46
Marek <mstanekofficial@gmail.com>
2022-11-19 14:38:05 +0100

    main added

---
+++ main.py
@@ -0,0 +1,4 @@
+import twitter as ts
+if __name__ == '__main__':
+    results = ts.getRecentTweets("python")
+    print(results)
--- twitter.py
+++ twitter.py
@@ -1,7 +1,7 @@
 from pytwitter import Api
 import twitter_bearer
 
-def getRecentTweets(query: str, no_of_tweets: int):
+def getRecentTweets(query: str, no_of_tweets: int = 10):
     api = Api(bearer_token=twitter_bearer.bearerCode)
     result = api.search_tweets(query = f"({query}) lang:pl", max_results= no_of_tweets)
     return [tweet.text for tweet in result.data]
