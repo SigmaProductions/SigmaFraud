b5161573fc91a7a89a0ec64730c621f23aa1b1aa
Marek <mstanekofficial@gmail.com>
2022-11-20 01:09:26 +0100

    cors

--- api.py
+++ api.py
@@ -4,9 +4,10 @@ import twitter as ts
 import nlp as pipeline
 from flask import request
 from bson import json_util, ObjectId
-
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 app.config["MONGO_URI"] = "mongodb://mongo:mongo@mongo:27017/local?authSource=admin"
 
 mongo = PyMongo(app)
@@ -19,10 +20,6 @@ for tweet in list_of_tweets:
         mongo.db.posts.insert_one(tweet.to_dict())
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
 @app.route("/susposts")
 def get_all_sus_posts():
     results = list(mongo.db.posts.find({}))
@@ -32,9 +29,7 @@ def get_all_sus_posts():
 
 @app.route("/changestate", methods = ['POST'])
 def set_post_state():
-    print(request.form)
     postId = request.form.get('id')
-    print(postId)
     state = request.form.get('state')
     mongo.db.posts.update_one({"_id" : ObjectId(postId)}, {"$set": {'state': state}})
     return {}
